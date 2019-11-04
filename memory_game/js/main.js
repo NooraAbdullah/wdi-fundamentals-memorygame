530174df0296350c0637314f1974970a1f769a30
Yazeed <Yazeed@Yazeeds-MBP.lan>
2019-11-04 10:30:13 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,13 +2,52 @@
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+
+
+{ rank      : "queen",
+  suit      : "hearts",
+  cardImage : "images/queen-of-hearts.png",
+
+ },
+
+// *******
+
+{ rank      : "queen",
+  suit      : "diamonds",
+  cardImage : "images/queen-of-diamonds.png",
+
+ },
+
+// *******
+
+ { rank      : "king",
+  suit      : "hearts",
+  cardImage : "images/king-of-hearts.png",
+
+ },
+
+// *******
+
+ { rank      : "king",
+  suit      : "diamonds",
+  cardImage : "images/king-of-diamonds.png",
+
+ }
+
+
+
+];
+
+
+/* -------------------------------------------------- End of Cards Array -------------------------------------------------- */
+
 
 var cardsInPlay = [];
 
 
 
-/* -------------------------------------------------- First Function -------------------------------------------------- */
+/* -------------------------------------------------- checkForMatch Function -------------------------------------------------- */
 
 
 
@@ -23,14 +62,14 @@ if(cardsInPlay.length === 2) {
 
 
 if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 }
 
 
 
 
 else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 
 
@@ -39,20 +78,21 @@ else {
 }
 
 
-/* -------------------------------------------------- Second Function -------------------------------------------------- */
+/* -------------------------------------------------- flipCard Function -------------------------------------------------- */
 
 
 
-function flipCard(cardId) {
+function flipCard (cardId) {
 
 
 
 
-console.log("User flipped "+ cards[cardId] );
+console.log("User flipped "+ cards[cardId].rank );
 
-cardsInPlay.push(cards[cardId])
+cardsInPlay.push(cards[cardId].rank);
 
-
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 
 checkForMatch();  
 
